b5973d56f7588bbb33c8c3a19f91eebb2976bc7b
Laine Taffin Altman <alexanderaltman@me.com>
2025-02-28 10:02:10 -0800

    Add impl start

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
+use std::ops::*;
+use num_traits::*;
+
 pub type Digit = u64;
 
+#[derive(Clone, Debug, Default, PartialEq, Eq, Hash)]
 pub struct BigInt {
-    
+    digits: Box<[Digit]>,
+}
+
+impl Add<&BigInt> for &BigInt {
+    type Output = BigInt;
+
+    fn add(self, other: &BigInt) -> Self::Output {
+        todo!()
+    }
 }
 
 #[cfg(test)]
